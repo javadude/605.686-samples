b9ee3294b5210731386149e992041fa9e9635838
Scott Stanchfield <scott@javadude.com>
2019-04-24 23:57:48 -0400

    Made example9 show how to use lazy instantiation for the DB setup

--- Module - Data Binding/DATABINDINGv2/app/src/main/java/com/javadude/databinding2/example09/SampleApplication.kt
+++ Module - Data Binding/DATABINDINGv2/app/src/main/java/com/javadude/databinding2/example09/SampleApplication.kt
@@ -6,11 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 class SampleApplication : Application() {
-    lateinit var db : Database
-
-    override fun onCreate() {
-        super.onCreate()
-        db = Room.databaseBuilder(this, Database::class.java, "PEOPLE")
+    val db by lazy {
+        Room.databaseBuilder(this, Database::class.java, "PEOPLE")
             .addCallback(object : RoomDatabase.Callback() {
                 override fun onCreate(db: SupportSQLiteDatabase) {
                     db.execSQL("INSERT INTO Address (id, ownerId, street, city, state, zip) VALUES('a1', 'p1', '123 Sesame Street', 'Laurel', 'MD', '20923')")
